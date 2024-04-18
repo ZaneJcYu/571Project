c6924978314a355b440d0d6b8cdf29a0b6c8d097
Jincheng <zaneyubbo@gmail.com>
2024-04-17 18:07:55 -0700

    Rough visualization

--- Visual.R
+++ Visual.R
@@ -7,14 +7,36 @@ Mdata <- read.csv('Mario Kart Data - Merged.csv')
 colnames(Mdata) <- c('Round', 'Race', 'Race Name', 'Vehicle', 'Type(C/B)', 'Place', 
                      'Completion Time', 'Player', 'Completion Time(s)')
 
-## Single player plot
+## Different Players
+Allplayers <- ggplot(data=Mdata, mapping = aes(x=Round, y=`Completion Time(s)`,color=`Race Name`,shape=`Vehicle`)) + 
+  geom_point(size = 3, alpha=0.6)
+Allplayers
+  
 SingplePlayer <- ggplot(data=Mdata, mapping = aes(x=Round, y=`Completion Time(s)`,color=`Race Name`)) + 
-  geom_point(size = 4, alpha=0.7)+
+  geom_point(size = 3, alpha=0.6)+
   facet_grid( ~ Player)
-
 SingplePlayer 
 
-## Different Races
-DifRace <- ggplot(data=Mdata, mapping = aes(x=Round, y=`Completion Time(s)`,color=`Race Name`)) + 
-  geom_point(size = 4, alpha=0.7)+
-  facet_grid( ~ Player)
+## Different cars
+Difcars <- ggplot(data=Mdata, mapping = aes(x=Round, y=`Completion Time(s)`,color=`Race Name`)) + 
+  geom_point(size = 3, alpha=0.6)+s
+  facet_grid( ~ `Vehicle`)
+Difcars
+
+## Different race
+### different maps with people
+DifRP <- ggplot(data=Mdata, mapping = aes(x=Round, y=`Completion Time(s)`,color=`Player`)) + 
+  geom_point(size = 3, alpha=0.6)+
+  facet_grid( ~ `Race Name`)
+DifRP
+### different maps with vehicle
+DifRC <- ggplot(data=Mdata, mapping = aes(x=Round, y=`Completion Time(s)`,color=`Vehicle`)) + 
+  geom_point(size = 3, alpha=0.6)+
+  facet_grid( ~ `Race Name`)
+DifRC
+
+### different maps with vehicle type
+DifRCT <- ggplot(data=Mdata, mapping = aes(x=Round, y=`Completion Time(s)`,color=`Type(C/B)`)) + 
+  geom_point(size = 3, alpha=0.6)+
+  facet_grid( ~ `Race Name`)
+DifRCT
